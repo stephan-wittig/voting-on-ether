0015d9bacb096d4d938a8c26e833fd78a5a3788a
Stephan Wittig <ste.wittig@gmail.com>
2018-10-25 17:20:43 +0200

    Ist jetzt responsive

--- src/components/contractData/ContractData.js
+++ src/components/contractData/ContractData.js
@@ -6,23 +6,53 @@ class ContractData extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      dataKey: null,
-      arguments: []
+      dataKey: null
     };
-
-    if (this.props.arguments) {
-      this.state.arguments = this.props.arguments;
-    }
+    this.fetchDataKey = this.fetchDataKey.bind(this);
   }
 
-  componentDidMount() {
-    const drizzle = this.props.drizzle;
-    const contract = drizzle.contracts[this.props.contract];
-
-    const dataKey = contract.methods[this.props.method].cacheCall(...this.state.arguments);
+  fetchDataKey(_method, _arguments = []) {
+    const dataKey = _method.cacheCall(..._arguments);
     this.setState({
       dataKey: dataKey
     })
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    var contract = this.props.drizzle.contracts[this.props.contract];
+    const method = contract.methods[this.props.method];
+
+    var nextContract = nextProps.drizzle.contracts[nextProps.contract];
+    const nextMethod = nextContract.methods[nextProps.method];
+
+    // Wenn sich die Methode oder die Argumente geändert haben, wird ein neuer cacheCall durchgeführt
+    if(
+      this.props.arguments !== nextProps.arguments ||
+      method !== nextMethod
+    ){
+      this.fetchDataKey(nextMethod, nextProps.arguments);
+    }
+
+    contract = this.props.drizzleState.contracts[this.props.contract];
+    const data = contract[this.props.method][this.state.dataKey];
+
+    nextContract = nextProps.drizzleState.contracts[nextProps.contract];
+    const nextData = nextContract[nextProps.method][nextState.dataKey];
+
+    // Wenn sich die hinter dem Key hinterlegten Daten ändern, wird die Komponente neu gerendert
+    if(data !== nextData){
+      return true;
+    }
+
+    // Ansonsten: Render die Komponente nicht neu
+    return false;
+  }
+
+  componentDidMount() {
+    var contract = this.props.drizzle.contracts[this.props.contract];
+    const method = contract.methods[this.props.method];
+
+    this.fetchDataKey(method, this.props.arguments);
   }
 
   render() {
